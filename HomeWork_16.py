0d72fdc6eaba92f3adc7934e74605e23ba6723fb
YuriyMihaylovichUsenko <usenkoyuriymih@gmail.com>
2022-07-23 01:02:53 +0300

    Completed HW16

--- HomeWork_16.py
+++ HomeWork_16.py
@@ -97,11 +97,11 @@ class Store:
         if identifier_type == 'name':
             for prod in self.all_products:
                 if prod.name == identifier:
-                    prod.price *= (1 - percent/100)
+                    prod.price *= (1 - percent / 100)
         elif identifier_type == 'type':
             for prod in self.all_products:
                 if prod.type == identifier:
-                    prod.price *= (1 - percent/100)
+                    prod.price *= (1 - percent / 100)
 
     def sell_product(self, product_name, amount):
         for prod in self.all_products:
@@ -116,9 +116,25 @@ class Store:
         for prod in self.all_products:
             print(prod.name, prod.type, prod.price, prod.amount)
 
-    @staticmethod
-    def get_product_info(product):
-        return product.name, product.amount
+    def get_product_info(self, product_name):
+        checking_exist = ''
+        for prod in self.all_products:
+            if prod.name == product_name:
+                checking_exist += 'something'
+                return prod.name, prod.amount
+        if not checking_exist:
+            raise ValueError('Such product does not exist')
+
+        # Як перевірити чи є в self.all_products продукт
+        # з product.name = product_name
+
+
+# Task 4
+class CustomException(BaseException):
+    def __init__(self, msg):
+        super().__init__(msg)
+        with open('logs.txt', 'a') as f:
+            f.write(msg + '\n')
 
 
 def main():
@@ -133,19 +149,15 @@ def main():
     #       m.filter_leaps([2001, 1884, 1995, 2003, 2020]) == [1884, 2020])
 
     # Task 3
-    my_store = Store(p1, p2)
+    # my_store = Store()
+    # my_store.add_product(p1, 10)
+    # my_store.add_product(p2, 300)
+    # my_store.sell_product('Ramen', 10)
+    #
+    # print(my_store.get_product_info('Ramen') == ('Ramen', 290))
 
-    my_store.add_product(p3, 100)
-    my_store.get_all_products()
-    my_store.add_product(p3, 100)
-    print(p3.amount)
-    print(p3.price)
-    my_store.set_discount('Food', 20, 'type')
-    print(p3.price)
-    my_store.sell_product('Football T-Shirt', 1)
-    my_store.sell_product('Bread', 20)
-    print(my_store.get_income())
-    print(my_store.get_product_info(p1))
+    # Task 4
+    raise CustomException("Some massage")
 
 
 if __name__ == '__main__':
