2522467d0b584aa941b72186dbe09a95fdaa19d3
YuriyMihaylovichUsenko <usenkoyuriymih@gmail.com>
2022-09-07 01:43:34 +0300

    HW 37

--- Server-Client/HW_36_Client_TCP.py
+++ Server-Client/HW_36_Client_TCP.py
@@ -1,3 +1,4 @@
+import json
 import socket
 
 PORT = 8001
@@ -13,19 +14,17 @@ client.connect((SERVER, PORT))
 
 def send(message, key='1'):
     """ функція для відправки повідомлень на серевер"""
-    message = message.encode(FORMAT)
-    key = key.encode(FORMAT)
-    message_length = len(message)
+    message_key_json = json.dumps({'message': message, 'key': key})
+    bytes_ = bytes(message_key_json, 'utf-8')
+    message_length = len(bytes_)
     send_length = str(message_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
     client.send(send_length)
-    send_length_key = str(len(key)).encode(FORMAT)
-    client.send(send_length_key)
-    client.send(key)
+    message = message_key_json.encode(FORMAT)
     client.send(message)
     print(client.recv(2048).decode(FORMAT))
 
 
-while (message := input('Введіть повідомлення: ')) != DISCONNECT_MESSAGE:
+while (message := input('Введіть повідомлення: ')):
     key = input('Введіть ключ: ')
     send(message, key)
--- Server-Client/HW_36_Server_TCP.py
+++ Server-Client/HW_36_Server_TCP.py
@@ -1,21 +1,9 @@
-def encrypt(text, s):
-    result = ""
-    for i in range(len(text)):
-        char = text[i]
-        # Encrypt uppercase characters in plain text
-        if char.isupper():
-            result += chr((ord(char) + s - 65) % 26 + 65)
-        # Encrypt lowercase characters in plain text
-        else:
-            result += chr((ord(char) + s - 97) % 26 + 97)
-    return result
-
+import json
 import socket
 import threading
 
 PORT = 8001  # ПОРТ ЩО МИ ВІДКРИЄМО ДЛЯ РОБОТИ
 SERVER = socket.gethostbyname(socket.gethostname())
-# print(SERVER)
 
 ADDRESS = (SERVER, PORT)
 
@@ -29,37 +17,49 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(ADDRESS)
 
 
+def encrypt(text, s):
+    result = ""
+    for i in range(len(text)):
+        char = text[i]
+        # Encrypt uppercase characters in plain text
+        if char.isupper():
+            result += chr((ord(char) + s - 65) % 26 + 65)
+        # Encrypt lowercase characters in plain text
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+    return result
+
+
 def handle_client(connection, address):
     # ця функція буде використовуватися для обробки зв'язку з сервером клієнту
     print(f'NEW CONNECTION - {address}')
     connected = True
     while connected:
-        # recv кількість байтів що отримає клієнт
+        # recv(кількість байтів що отримає клієнт)
         # Але ми не можемо знати завчасно скільки отримаємо
-        # message = connection.recv(HEADER)
         # тому варто створити хедер
         # раз ми не знаємо розмір повідомлення, що буде відправлено на сервер
-        # то ми будемо спочатку відправляти такзваний хедер,
-        # в якому ми будемо вказувати розмір повідомлення, а потім
+        # то ми будемо спочатку відправляти розмір повідомлення.
+        # в recv будемо вказувати хедер, а потім
         # будемо відправляти саме повідомлення
-        # 11 "hello world"
         message_length = connection.recv(HEADER).decode(FORMAT)
-        # if message_length:  # later after client first try
-        message_length = int(message_length)
-        message = connection.recv(message_length).decode(FORMAT)
-        # key_length = connection.recv(HEADER).decode(FORMAT)
-        # key = int(connection.recv(key_length).decode(FORMAT))
+        if message_length:  # later after client first try
+            message_length = int(message_length)
+            message_json = connection.recv(message_length).decode(FORMAT)
+            message_dict = json.loads(message_json)
+            key = int(message_dict.get('key', 1))
+            message = message_dict.get('message', 'uppps')
 
-        # далі маємо потурбуватися щоб з'єднання було закрито
-        if message == DISCONNECT_MESSAGE:
-            connected = False
+            # далі маємо потурбуватися, щоб з'єднання було закрито
+            if message == DISCONNECT_MESSAGE:
+                connected = False
 
         print(f'[{address}] - {message}')
         print(key)
-        # У же після того як показав зв'язок з декількома клієнтами
-        # зробимо так, що сервер теж щось відповідав
-        connection.send(f'Message received from {address}'.encode(FORMAT))
-
+        # Тепер сервер відправляє закодоване повідомлення
+        connection.send(
+            f'Your encrypt message {encrypt(message, key)} '.encode(FORMAT)
+        )
     connection.close()
 
 
